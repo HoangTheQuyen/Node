b3c926ff1d34caed4c20abf8bb8c9a0b3d21c032
hoang the quyen <hoangthequyen01@gmail.com>
2019-05-10 16:57:00 +0700

    finish set-up project

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// Internal Dependencies
+const MongoConfig = require("./config/mongoConfig.js");
+const _DefaultRoutes = require("./config/defaultRoutes");
+const _Routes = require("./routes/index");
+
+
+
+// Main app
+const app = express();
+
+// Express configuration
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+// Main APIs
+app.use(_DefaultRoutes.BASE_URL, _Routes);
+
+mongoose.connect(MongoConfig.CONNECTION_STRING, {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection: error!"));
+db.once("open", () => {
+  console.log("[MongoDB] Connected!");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+// Start server
+const server = app.listen(3000, () => {
+  console.log(`[PORT:${server.address().port}] Server is running...`);
+});
---
+++ config/defaultRoutes.js
@@ -0,0 +1,58 @@
+module.exports = {
+    BASE_URL: '/api',
+    AUTHENTICATION:{
+        ROOT:'/auth',
+        LOGIN:'/login',
+        LOGOUT:'/logout',
+        REGISTER:'/register',
+        RESETPASSWORD: '/resetpassword'
+    },
+    USER:{
+        ROOT:'/user',
+        GET_ALL: '/all',
+        GET_ONE: '/one',
+        CREATE:'/create',
+        UPDATE: '/update',
+        REMOVE:'/remove'
+    },
+    PRODUCT: {
+        ROOT: '/product',
+        GET_ALL: '/all',
+        GET_ONE: '/one',
+        CREATE:'/create',
+        UPDATE: '/update',
+        REMOVE:'/remove'
+    },
+    SUPPLIER:{
+        ROOT:'/supplier',
+        GET_ALL: '/all',
+        GET_ONE: '/one',
+        CREATE:'/create',
+        UPDATE: '/update',
+        REMOVE:'/remove'
+    },
+    PURCHASEORDER:{
+        ROOT:'/order',
+        GET_ALL: '/all',
+        GET_ONE: '/one',
+        CREATE:'/create',
+        UPDATE: '/update',
+        REMOVE:'/remove'
+    },
+    SALEORDER:{
+        ROOT:'/order',
+        GET_ALL: '/all',
+        GET_ONE: '/one',
+        CREATE:'/create',
+        UPDATE: '/update',
+        REMOVE:'/remove'
+    },
+    CARDITEM:{
+        ROOT:'/carditem',
+        GET_ALL: '/all',
+        GET_ONE: '/one',
+        CREATE:'/create',
+        UPDATE: '/update',
+        REMOVE:'/remove'
+    }
+}
